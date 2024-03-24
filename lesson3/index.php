255f1fe61b7e35ea5c6e69931e9bf67a8a631233
123 <sobolivsckaya@yandex.ru>
2024-03-24 10:50:38 +0300

    commit

--- lesson3/index.php
+++ lesson3/index.php
@@ -103,8 +103,8 @@ if ($errors != '') {
   errp($errors);
 }
 
-$db = new PDO('mysql:host=localhost;dbname=u67360', '4232624',
-   [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+$db = new PDO('mysql:host=localhost;dbname=u67360', 'u67360', '4232624',
+     [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 
 $inQuery = implode(',', array_fill(0, count($lang), '?'));
 
